be9f357fcd78ce6f00f5e223ca1297712c8db44f
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2022-01-23 11:11:49 -0300

    Aula 06

---
+++ Aula06/app_dart/bin/app_dart.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  if (arguments[0] == "jaque") {
+    print("Você escolheu Jaque");
+  } else if (arguments[0] == "imprimir") {
+    print("executando impressao");
+    print(arguments);
+  }
+  //para rodar via git bash , entrar dentro de bin->app_dart.dart
+  //e escrever dart nome do arquivo
+}
